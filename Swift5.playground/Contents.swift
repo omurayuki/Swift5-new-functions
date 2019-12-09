f0ce39b81a6450826a03e30fa081f0dcc91a9678
omurayuki <g1221438qz12@gmail.com>
2019-12-09 22:53:33 +0900

    dynamic member lookup, dynamicallyCallable 実装

--- Swift5.playground/Contents.swift
+++ Swift5.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 /// Result
+/// Reference materials: https://qiita.com/guitar_char/items/84477337b11c11a4f5f6
 
 enum NetworkError: Error {
     case badURL
@@ -34,11 +35,8 @@ fetchUnreadCount(from: "https://www.hackingwithswift.com") { result in
 
 
 
-
-
-
-
-//// Raw Strings
+/// Raw Strings
+/// Reference materials: https://medium.com/@d_date/whats-new-in-swift-5-70225f063b87
 
 let rain = #"the "rain" in "Spain" falls mainly on the Spaniards."#
 
@@ -56,3 +54,68 @@ let multiline = #"""
 let regex1 = "\\\\[A-Z]+[A-Za-z]+\\.[a-z]+"
 // ↓
 let regex2 = #"\\[A-Z]+[A-Za-z]+\.[a-z]+"#
+
+
+
+
+
+
+/// @dynamicMemberLookup
+/// Reference materials: https://dev.classmethod.jp/smartphone/playground-dynamic-member-lookup-dynamiccallable/
+
+enum KeySwitch {
+    case linear
+    case tactile
+    case clicky
+}
+ 
+@dynamicMemberLookup
+class MechanicalKeyboard {
+    let keySwitch: KeySwitch
+    let name: String
+    let numberOfKeys: Int
+ 
+    init(keySwitch: KeySwitch, name: String, number: Int) {
+        self.keySwitch = keySwitch
+        self.name = name
+        self.numberOfKeys = number
+    }
+ 
+    subscript(dynamicMember key: String) -> Any {
+        switch key {
+        case "switch":
+            return keySwitch
+        case "type":
+            return "This keyboarwd is \(name)"
+        case "numbers":
+            return numberOfKeys
+        default:
+            return ""
+        }
+    }
+}
+ 
+let keyboard = MechanicalKeyboard(keySwitch: .tactile, name: "kbd67", number: 67)
+print(keyboard.switch)
+print(keyboard.type)
+print(keyboard.numbers)
+
+
+
+
+
+
+/// @dynamicCallable
+/// Reference materials: https://qiita.com/tamappe/items/64fbe4d0991cd222540e
+
+@dynamicCallable
+struct RandomNumberGenerator {
+    func dynamicallyCall(withArguments args: [Int]) -> Double {
+        let numberOfZeroes = Double(args[0])
+        let maximum = pow(10, numberOfZeroes)
+        return Double.random(in: 0...maximum)
+    }
+}
+
+let random = RandomNumberGenerator()
+let result = random(0)
